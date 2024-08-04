05880a5f60d12c22c434cd731870f7b916bea97b
Dbacksfanatic <dbacksfanatic@cox.net>
2024-08-03 19:24:07 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "1070675873965150208"; // your league ID
-export const leagueName = "the_goat_superflex_contenders"; // your league name
+export const leagueName = "The G.O.A.T. Superflex Contenders"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
